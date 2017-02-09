0f9100efde60473f3162587d3f74a85630e7d47f
JiangShuai <825663508@qq.com>
2017-02-09 10:31:13 +0800

    配置
    配置工程

---
+++ TestQQChat/TestQQChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestQQChat
+//
+//  Created by aigegou on 2017/2/9.
+//  Copyright © 2017年 aigegou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
