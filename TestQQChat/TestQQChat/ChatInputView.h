e4b1a3f8257598d81aebbf8da7950c57b52180b3
JiangShuai <825663508@qq.com>
2017-02-15 17:27:11 +0800

    修改部分界面逻辑对键盘进行调整

--- TestQQChat/TestQQChat/ChatInputView.h
+++ TestQQChat/TestQQChat/ChatInputView.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+//typedef void (^SendMessage)(NSString *message);
+
+@protocol UserSenderInfoDelegate <NSObject>
+
+-(void)sendMessage:(NSString *)message;
+
+@end
+
 @interface ChatInputView : UIView
+
+@property (nonatomic, weak) id <UserSenderInfoDelegate> delegate;
+
+//@property (nonatomic, copy) SendMessage message;
 
 @end
